b5a7ff303c5e1790f06fc10cc9447303aeb9a6f3
Abhilash Jena <ajena@modeln.com>
2018-04-26 20:33:49 +0530

    updated partitioning scripts

--- sqls/create_partitions_new2.sql
+++ sqls/create_partitions_new2.sql
@@ -68,79 +68,79 @@ DECLARE
 BEGIN
 
     IF LENGTH(sf_namespace) = 0 THEN
-        RAISE NOTICE '%: namespace not specified', now();
+        RAISE NOTICE '%: namespace not specified', timeofday();
     ELSE
-        RAISE NOTICE '%: namespace: %', now(), sf_namespace;
+        RAISE NOTICE '%: namespace: %', timeofday(), sf_namespace;
     END IF;
 
 
     --  1. Pause INSERT/DELETE on mnadvsimulation__c table.
-    RAISE NOTICE '%: Pausing INSERT/DELETE on %', now(), master_table_name;
+    RAISE NOTICE '%: Pausing INSERT/DELETE on %', timeofday(), master_table_name;
     script_text := 'CREATE RULE ' || master_table_name || '_insert_disable AS ON INSERT TO gpm.' || master_table_name || ' DO INSTEAD NOTHING';
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text;
     script_text := 'CREATE RULE ' || master_table_name || '_delete_disable AS ON DELETE TO gpm.' || master_table_name || ' DO INSTEAD NOTHING';
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text;
 
 
     --  2. Verify that all records in mnadvsimulation__c has a createddate.
-    RAISE NOTICE '%: Verifying data in %...', now(), sim_table_name;
+    RAISE NOTICE '%: Verifying data in %...', timeofday(), sim_table_name;
     script_text := 'SELECT COUNT(*) FROM gpm.' || sim_table_name || ' WHERE createddate IS NULL';
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text INTO row_count;
-    RAISE NOTICE '%: %', now(), row_count;
+    RAISE NOTICE '%: %', timeofday(), row_count;
     IF row_count > 0 THEN
-        RAISE EXCEPTION '%: % rows in % have NULL createddate. Please re-verify/clean up the data before proceeding again.', now(), row_count, sim_table_name;
+        RAISE EXCEPTION '%: % rows in % have NULL createddate. Please re-verify/clean up the data before proceeding again.', timeofday(), row_count, sim_table_name;
     END IF;
 
 
     --  3. Create a backup of mnadvsimulationoutput__c table.
     --     Name the new table like this - mnadvsimulationoutput__c_bak_<current_datetime>.
-    RAISE NOTICE '%: Creating backup of %', now(), master_table_name;
+    RAISE NOTICE '%: Creating backup of %', timeofday(), master_table_name;
     script_text := 'CREATE TABLE gpm.' || master_table_name || '_bak_' || TO_CHAR(now(), 'YYYYMMDDHH24MISS') || ' AS '
                 || 'SELECT * FROM gpm.' || master_table_name;
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text;
 
 
     --  4. Create a new table (without data) from mnadvsimulationoutput__c table.
-    RAISE NOTICE '%: Creating a structural copy of %', now(), master_table_name;
+    RAISE NOTICE '%: Creating a structural copy of %', timeofday(), master_table_name;
     script_text := 'CREATE TABLE gpm.' || new_master_table_name
                 || '(LIKE gpm.' || master_table_name || ')';
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text;
 
 
     --  5. Add column mnadvsimcreateddate__c to mnadvsimulationoutput__c_new to capture the simulation scenario creation date.
     --     This new column will act as the partition key for the quarterly range partition.
-    RAISE NOTICE '%: Adding column % to %', now(), date_range_part_col_name, new_master_table_name;
+    RAISE NOTICE '%: Adding column % to %', timeofday(), date_range_part_col_name, new_master_table_name;
     script_text := 'ALTER TABLE gpm.' || new_master_table_name || ' '
                 || 'ADD COLUMN ' || date_range_part_col_name || ' timestamp without time zone';
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text;
 
 
     --  6. Starting from the earliest quarter(based on least createddate in mnadvsimulation__c table) till current quarter,
     --     - create child tables(with PK constraint) of mnadvsimulationoutput__c_new master table.
     --     - insert appropriate rows into the child table
-    RAISE NOTICE '%: Start creation of child tables.', now();
+    RAISE NOTICE '%: Start creation of child tables.', timeofday();
     -- Determine start year/quarter
-    RAISE NOTICE '%: Determine start year/quarter.', now();
+    RAISE NOTICE '%: Determine start year/quarter.', timeofday();
     script_text := 'SELECT createddate FROM gpm.' || sim_table_name || ' ORDER BY createddate LIMIT 1';
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text INTO min_createddate;
     start_year := EXTRACT(YEAR FROM min_createddate);
     start_quarter := EXTRACT(QUARTER FROM min_createddate);
-    RAISE NOTICE '%: %', now(), start_year || ' Q' || start_quarter;
+    RAISE NOTICE '%: %', timeofday(), start_year || ' Q' || start_quarter;
 
     -- Determine end year/quarter
-    RAISE NOTICE '%: Determine end year/quarter.', now();
+    RAISE NOTICE '%: Determine end year/quarter.', timeofday();
     curr_year := extract(year FROM now());
     curr_quarter := extract(quarter FROM now());
-    RAISE NOTICE '%: %', now(), curr_year || ' Q' || curr_quarter;
+    RAISE NOTICE '%: %', timeofday(), curr_year || ' Q' || curr_quarter;
 
-    RAISE NOTICE '%: Creating child tables starting from year & quarter : %', now(), start_year || ' Q' || start_quarter;
+    RAISE NOTICE '%: Creating child tables starting from year & quarter : %', timeofday(), start_year || ' Q' || start_quarter;
     WHILE (start_year * 10 + start_quarter) <= (curr_year * 10 + curr_quarter) LOOP
         partition_start_date = TO_CHAR(TO_DATE(start_year || '-' || (((start_quarter - 1) * 3) + 1) || '-1', 'YYYY-MM-DD'), 'YYYY-MM-DD');
         partition_next_start_date = TO_CHAR(partition_start_date + interval '3 months', 'YYYY-MM-DD');
@@ -150,16 +150,16 @@ BEGIN
 
         -- Create child table
         child_table_name := master_table_name || '_' || start_year || 'q' || start_quarter;
-        RAISE NOTICE '%: Creating child table', now();
+        RAISE NOTICE '%: Creating child table', timeofday();
         script_text := 'CREATE TABLE gpm.' || child_table_name || '('
                     -- PK Constraint
                     || 'CONSTRAINT ' || child_table_name || '_pkey PRIMARY KEY (id)'
                     || ') INHERITS (gpm.' || new_master_table_name || ');';
-        RAISE NOTICE '%: %', now(), script_text;
+        RAISE NOTICE '%: %', timeofday(), script_text;
         EXECUTE script_text;
 
         -- Insert rows
-        RAISE NOTICE '%: Inserting rows into child table', now();
+        RAISE NOTICE '%: Inserting rows into child table', timeofday();
         part_data_sel_date_constraint_text := 'asim.' || master_table_part_col_name || ' >= TO_DATE(''' || partition_start_date || ''', ''YYYY-MM-DD'') '
                               || 'AND ' || 'asim.' || master_table_part_col_name || ' < TO_DATE(''' || partition_next_start_date || ''', ''YYYY-MM-DD'')';
         script_text := 'INSERT INTO gpm.' || child_table_name || ' '
@@ -167,7 +167,7 @@ BEGIN
                     || 'FROM gpm.' || master_table_name || ' aso '
                     || 'INNER JOIN gpm.' || sim_table_name || ' asim ON aso.' || sim_table_fk_name || ' = asim.sfid '
                     || 'WHERE ' || part_data_sel_date_constraint_text;
-        RAISE NOTICE '%: %', now(), script_text;
+        RAISE NOTICE '%: %', timeofday(), script_text;
         EXECUTE script_text;
 
         -- Increment Quarter
@@ -178,36 +178,36 @@ BEGIN
         END IF;
     END LOOP;
 
-    RAISE NOTICE '%: Child tables created till year & quarter : %', now(), curr_year || ' Q' || curr_quarter;
+    RAISE NOTICE '%: Child tables created till year & quarter : %', timeofday(), curr_year || ' Q' || curr_quarter;
 
 
     --  7. Verify partitioned data integrity
-    RAISE NOTICE '%: Running Verification...', now();
+    RAISE NOTICE '%: Running Verification...', timeofday();
     script_text := 'SELECT COUNT(1) '
                 || 'FROM gpm.' || master_table_name || ' aso '
                 || 'INNER JOIN gpm.' || sim_table_name || ' asim ON aso.' || sim_table_fk_name || ' = asim.sfid';
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text INTO source_row_count;
-    RAISE NOTICE '%: %', now(), source_row_count;
+    RAISE NOTICE '%: %', timeofday(), source_row_count;
     script_text := 'SELECT COUNT(1) '
                 || 'FROM gpm.' || new_master_table_name;
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text INTO row_count;
-    RAISE NOTICE '%: %', now(), row_count;
+    RAISE NOTICE '%: %', timeofday(), row_count;
     IF row_count != source_row_count THEN
-        RAISE EXCEPTION '%: Row count mis-match after partitioning.', now();
+        RAISE EXCEPTION '%: Row count mis-match after partitioning.', timeofday();
     END IF;
     script_text := 'SELECT COUNT(*) FROM ONLY gpm.' || new_master_table_name;
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text INTO row_count;
-    RAISE NOTICE '%: %', now(), row_count;
+    RAISE NOTICE '%: %', timeofday(), row_count;
     IF row_count > 0 THEN
-        RAISE EXCEPTION '%: After paritioning, table % should not contain any records.', now(), new_master_table_name;
+        RAISE EXCEPTION '%: After paritioning, table % should not contain any records.', timeofday(), new_master_table_name;
     END IF;
 
 
     --  8. Create Indexes, INSERT rules and CHECK constraints.
-    RAISE NOTICE '%: Creating INSERT Rules and Check constraints.', now();
+    RAISE NOTICE '%: Creating INSERT Rules and Check constraints.', timeofday();
     -- Reset variables for re-iteration
     start_year := EXTRACT(YEAR FROM min_createddate);
     start_quarter := EXTRACT(QUARTER FROM min_createddate);
@@ -219,9 +219,9 @@ BEGIN
                               || 'AND ' || date_range_part_col_name || ' < TO_DATE(''' || partition_next_start_date || ''', ''YYYY-MM-DD'')';
 
         -- Create indexes
-        RAISE NOTICE '%: Creating indexes', now();
+        RAISE NOTICE '%: Creating indexes', timeofday();
         child_table_name_for_index := sf_namespace || master_table_short_name || '_' || start_year || 'q' || start_quarter;
-        RAISE NOTICE '%: Creating Index - %', now(), 'idx_' || child_table_name_for_index || '_sim_countryorder_cpsku_cpt ON ' || child_table_name
+        RAISE NOTICE '%: Creating Index - %', timeofday(), 'idx_' || child_table_name_for_index || '_sim_countryorder_cpsku_cpt ON ' || child_table_name
                                         || '(' || sim_table_fk_name || ',' || sf_namespace || 'countryorder__c,'
                                         || sf_namespace || 'mncountryproductsku__c,' || sf_namespace || 'mncountrypricetype__c)';
         PERFORM gpm.create_index_if_not_exists(
@@ -233,7 +233,7 @@ BEGIN
             || sf_namespace || 'mncountrypricetype__c',
             'idx_' || child_table_name_for_index || '_sim_countryorder_cpsku_cpt'
         );
-        RAISE NOTICE '%: Creating Index - %', now(), 'idx_' || child_table_name_for_index || '_forecastdate_sim ON ' || child_table_name
+        RAISE NOTICE '%: Creating Index - %', timeofday(), 'idx_' || child_table_name_for_index || '_forecastdate_sim ON ' || child_table_name
                                            || '(' || sf_namespace || 'forecastdate__c,' || sim_table_fk_name || ')';
         PERFORM gpm.create_index_if_not_exists(
             'btree',
@@ -242,7 +242,7 @@ BEGIN
             || sim_table_fk_name,
             'idx_' || child_table_name_for_index || '_forecastdate_sim'
         );
-        RAISE NOTICE '%: Creating Index - %', now(), 'idx_' || child_table_name_for_index || '_partkey ON ' || child_table_name
+        RAISE NOTICE '%: Creating Index - %', timeofday(), 'idx_' || child_table_name_for_index || '_partkey ON ' || child_table_name
                                         || '(' || date_range_part_col_name || ')';
         PERFORM gpm.create_index_if_not_exists(
             'btree',
@@ -256,7 +256,7 @@ BEGIN
                     || 'ADD CONSTRAINT ' || child_table_name || '_ck CHECK ('
                     || check_constraint_text
                     || ')';
-        RAISE NOTICE '%: %', now(), script_text;
+        RAISE NOTICE '%: %', timeofday(), script_text;
         EXECUTE script_text;
 
         -- Create INSERT Rule
@@ -266,7 +266,7 @@ BEGIN
                     || ') '
                     || 'DO INSTEAD '
                     || 'INSERT INTO gpm.' || child_table_name || ' VALUES (NEW.*)';
-        RAISE NOTICE '%: %', now(), script_text;
+        RAISE NOTICE '%: %', timeofday(), script_text;
         EXECUTE script_text;
 
         -- Increment Quarter
@@ -279,30 +279,30 @@ BEGIN
 
 
     --  9. Rename tables so that mnadvsimulation__c_new becomes mnadvsimulation__c
-    RAISE NOTICE '%: Renaming tables.', now();
+    RAISE NOTICE '%: Renaming tables.', timeofday();
     script_text := 'ALTER TABLE gpm.' || master_table_name || ' RENAME TO ' || master_table_name || '_old';
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text;
     script_text := 'ALTER TABLE gpm.' || new_master_table_name || ' RENAME TO ' || master_table_name;
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text;
 
 
     -- 10. Turn on constraint_exclusion.
-    RAISE NOTICE '%: Turning on constraint exclusion', now();
+    RAISE NOTICE '%: Turning on constraint exclusion', timeofday();
     SET constraint_exclusion = on;
 
 
     -- 11. Resume INSERT/DELETE on mnadvsimulation__c table.
-    RAISE NOTICE '%: Resuming INSERT/DELETE on %', now(), master_table_name;
+    RAISE NOTICE '%: Resuming INSERT/DELETE on %', timeofday(), master_table_name;
     script_text := 'DROP RULE ' || master_table_name || '_insert_disable ON gpm.' || master_table_name || '_old';
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text;
     script_text := 'DROP RULE ' || master_table_name || '_delete_disable ON gpm.' || master_table_name || '_old';
-    RAISE NOTICE '%: %', now(), script_text;
+    RAISE NOTICE '%: %', timeofday(), script_text;
     EXECUTE script_text;
 
-    RAISE NOTICE '%: END OF PARTITION_MNADVSIMULATIONOUTPUT', now();
+    RAISE NOTICE '%: END OF PARTITION_MNADVSIMULATIONOUTPUT', timeofday();
 
 END;
 $PARTITION_MNADVSIMULATIONOUTPUT$ LANGUAGE plpgsql;
---
+++ sqls/create_test_table_for_part.sql
@@ -0,0 +1,48 @@
+DO $$
+DECLARE
+    sf_namespace VARCHAR := '';
+    master_table_name VARCHAR := sf_namespace || 'mnadvsimulationoutput__c';
+    test_table_name VARCHAR := master_table_name || '_testmnirp';
+    script_text VARCHAR;
+    tab_name VARCHAR;
+    row_count INTEGER;
+BEGIN
+    -- Drop test tables if exists
+    script_text := 'DROP TABLE IF EXISTS gpm.' || test_table_name || ' CASCADE';
+    RAISE NOTICE '%: %', timeofday(), script_text;
+    EXECUTE script_text;
+    script_text := 'SELECT table_name FROM information_schema.tables WHERE table_schema = ''gpm'' AND table_name LIKE ''' || test_table_name || '%''';
+    RAISE NOTICE '%: %', timeofday(), script_text;
+    FOR tab_name IN EXECUTE script_text
+        LOOP
+            script_text := 'DROP TABLE gpm.' || tab_name || ' CASCADE';
+            RAISE NOTICE '%', script_text;
+            EXECUTE script_text;
+        END LOOP;
+        
+    -- Create test tables
+    script_text := 'CREATE table gpm.' || test_table_name || ' as '
+                || 'SELECT * FROM gpm.' || master_table_name;
+    RAISE NOTICE '%: %', timeofday(), script_text;
+    EXECUTE script_text;
+    
+    -- Count rows
+    script_text := 'SELECT COUNT(*) FROM gpm.' || test_table_name;
+    RAISE NOTICE '%: %', timeofday(), script_text;
+    EXECUTE script_text INTO row_count;
+    RAISE NOTICE '%: %', timeofday(), row_count;
+
+    -- Create indexes
+    script_text := 'CREATE INDEX idx_mnadvsimoutputtest_forecastdate_sim ' 
+                || 'ON gpm.' || test_table_name || ' USING btree '
+                || '(' || sf_namespace || 'forecastdate__c, ' || sf_namespace || 'mnadvsimulation__c)';
+    RAISE NOTICE '%: %', timeofday(), script_text;
+    EXECUTE script_text;
+    
+    script_text := 'CREATE INDEX idx_mnadvsimoutputtest_sim_countryorder_cpsku_cpt '
+                || 'ON gpm.' || test_table_name || ' USING btree '
+                || '(' || sf_namespace || 'mnadvsimulation__c, ' || sf_namespace || 'countryorder__c, ' || sf_namespace || 'mncountryproductsku__c, ' || sf_namespace || 'mncountrypricetype__c)';
+    RAISE NOTICE '%: %', timeofday(), script_text;
+    EXECUTE script_text;
+END;
+$$ LANGUAGE plpgsql;
